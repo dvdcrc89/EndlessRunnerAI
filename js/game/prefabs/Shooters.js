395cab367b72572741db95055a3c23921010accc
dvdcrc89@gmail.com <dvdcrc89@gmail.com>
2018-11-27 18:48:23 +0000

    implements upgrade

--- js/game/prefabs/Shooters.js
+++ js/game/prefabs/Shooters.js
@@ -1,22 +1,27 @@
 Shooters = function(game) {
     this.game = game;
     this.fireballs = game.add.group();
+    this.isReady = true;
+    this.fireballTime = game.time.now
     this.upgrade= {
         shots:[{
             velX: -900,
-            velY: -200,
-            size: 0.1
+            velY: -60,
+            size: 0.3
             },{
             velX: -900,
             velY: 0,
-            size: 0.5
+            size: 0.3
             },{
             velX: -900,
-            velY: +200,
-            size: 0.1,
-            }]
+            velY: +60,
+            size: 0.3,
+            }],
+        shootRecall:100
     };
     this.shoot= function (x,y){
+        if(game.time.now > this.fireballTime){
+
         this.upgrade.shots.forEach(shot=>{
             let fireball = this.game.add.sprite(x, y, 'fireball');
             fireball.animations.add('shoot', [0, 1, 2, 3]);
@@ -31,8 +36,11 @@ Shooters = function(game) {
             this.fireballs.add(fireball);
  
         })
+         this.fireballTime=game.time.now+this.upgrade.shootRecall;                                                                         
+        }
     }
-                                   
+      
+
     }
     
     
--- js/game/states/Game.js
+++ js/game/states/Game.js
@@ -1,139 +1,64 @@
-JustRun.Game = function() {};
+JustRun.Game = function() {}
 JustRun.Game = {
+
     create: function() {
         this.score = 0;
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        this.background = game.add.tileSprite(
-            0,
-            0,
-            game.width,
-            game.height - 1,
-            "background"
-        );
+        this.background = game.add.tileSprite(0, 0, game.width, game.height - 1, 'background');
         this.background.autoScroll(-350, 0);
-        this.ground = game.add.tileSprite(
-            0,
-            game.height - 1,
-            game.width,
-            1,
-            "ground"
-        );
+        this.ground = game.add.tileSprite(0, game.height - 1, game.width, 1, 'ground');
         this.ground.autoScroll(-750, 0);
         this.coins = game.add.group();
         this.players = game.add.group();
-        this.shooters = new Shooters(this.game);
-        this.players.add(
-            new Pilot(
-                this.game,
-                0,
-                game.height - 50,
-                "pilot",
-                this.shooters.fireballs
-            )
-        );
-        this.players.add(
-            new Pilot(
-                this.game,
-                0,
-                game.height - 250,
-                "pilot",
-                this.shooters.fireballs
-            )
-        );
-        this.players.add(
-            new Pilot(
-                this.game,
-                0,
-                game.height - 450,
-                "pilot",
-                this.shooters.fireballs
-            )
-        );
-        this.players.add(
-            new Pilot(
-                this.game,
-                0,
-                game.height - 850,
-                "pilot",
-                this.shooters.fireballs
-            )
-        );
-        this.messages = game.add.group();
+        this.shooters = new Shooters(this.game)
+        this.players.add(new Pilot(this.game, 0, game.height - 50, 'pilot', this.shooters.fireballs));
+        this.players.add(new Pilot(this.game, 0, game.height - 250, 'pilot', this.shooters.fireballs));
+        this.players.add(new Pilot(this.game, 0, game.height - 450, 'pilot', this.shooters.fireballs));
+        this.players.add(new Pilot(this.game, 0, game.height - 850, 'pilot', this.shooters.fireballs));
+        this.messages = game.add.group()
         this.dead = game.add.group();
 
-        game.physics.enable([this.ground, this.players], Phaser.Physics.ARCADE);
-        game.physics.enable(this.dead, Phaser.Physics.ARCADE);
+        game.physics.enable([this.ground, this.players], Phaser.Physics.ARCADE)
+        game.physics.enable(this.dead, Phaser.Physics.ARCADE)
 
         this.ground.body.collideWorldBounds = true;
         this.ground.body.immovable = true;
         this.ground.body.allowGravity = false;
-        this.monster = new Monster(
-            this.game,
-            this.game.width - 100,
-            game.height,
-            "monster",
-            this.players,
-            this.shooters
-        );
+        this.monster = new Monster(this.game, this.game.width - 100, game.height, 'monster', this.players, this.shooters)
         this.youCanShot = true;
         this.combo = 0;
-        this.timer2 = game.time.events.loop(
-            1000,
-            () => {
-                if (this.players.length < 10000) {
-                    this.players.add(
-                        new Pilot(
-                            this.game,
-                            0,
-                            game.height - Math.random() * 300,
-                            "pilot",
-                            this.shooters.fireballs
-                        )
-                    );
-                }
-            },
-            this
-        );
-        this.timer3 = game.time.events.loop(
-            405,
-            () => (this.youCanShot = true),
-            this
-        );
-        this.timer4 = game.time.events.loop(
-            4000,
-            () => this.messages.children.map(t => t.destroy()),
-            this
-        );
-        this.timer5 = game.time.events.loop(1000, () => (this.combo = 0), this);
-        this.timer6 = game.time.events.loop(
-            10000,
-            () => (this.background.tint = Math.random() * 0xffffff),
-            this
-        );
+        this.timer2 = game.time.events.loop(1000, () => {
+            if (this.players.length < 10000) {
+                this.players.add(new Pilot(this.game, 0, (Math.random() * game.height ), 'pilot', this.shooters.fireballs))
+            }
+        }, this)
+        this.timer3 = game.time.events.loop(405, () => this.youCanShot = true, this)
+        this.timer4 = game.time.events.loop(4000, () => this.messages.children.map(t => t.destroy()), this)
+        this.timer5 = game.time.events.loop(1000, () => this.combo = 0, this)
+        this.timer6 = game.time.events.loop(10000, () => this.background.tint = Math.random() * 0xffffff, this)
 
-        this.coinAudio = game.add.audio("coinAudio");
-        this.ugradeAudio = game.add.audio("upgradeAudio");
-        this.hitAudio = game.add.audio("hitAudio");
+        this.coinAudio = game.add.audio('coinAudio');
+        this.ugradeAudio = game.add.audio('upgradeAudio');
+        this.hitAudio = game.add.audio('hitAudio');
         console.log(this.dog, this.player);
     },
     update: function() {
-        this.shooters.fireballs.children
-            .filter(fireball => fireball.x < 0)
-            .map(fireball => fireball.destroy());
+        this.shooters.fireballs.children.filter((fireball) => fireball.x < 0).map((fireball) => fireball.destroy());
         game.physics.arcade.collide(this.players, this.ground);
         game.physics.arcade.collide(this.dog, this.ground);
 
         this.players.children.map(player => player.play());
         this.monster.play();
 
-        game.physics.arcade.overlap(
-            this.shooters.fireballs,
-            this.players,
-            this.die,
-            null,
-            this
-        );
+
+
+
+        game.physics.arcade.overlap(this.shooters.fireballs, this.players, this.die, null, this);
+
+
+
+
     },
     die: function(fireball, player) {
         if (player.x > 50) {
@@ -142,7 +67,7 @@ JustRun.Game = {
             player.body.velocity.y = 400;
 
             this.combo++;
-            this.timer5 = game.time.events.loop(3000, () => (this.combo = 0), this);
+            this.timer5 = game.time.events.loop(3000, () => this.combo = 0, this)
             let style = {
                 font: "3rem Orbitron",
                 //            fill: "#FF4136",
@@ -150,18 +75,18 @@ JustRun.Game = {
             };
 
             this.messages.removeAll();
-            this.messages.add(
-                game.add.text(player.x, player.y, this.combo + " KILL!", style)
-            );
+            this.messages.add(game.add.text(player.x, player.y, this.combo + ' KILL!', style));
 
             this.timer5.delay += 2000;
             player.rotation += 2;
 
-            player.tint = 0xf42336;
+            player.tint = 0xF42336;
             player.body.collideWorldBounds = false;
             player.dead = true;
             this.players.remove(player);
             this.dead.add(player);
+
         }
+
     }
 };
