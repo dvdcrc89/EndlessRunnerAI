f818d25de0a346ebf298e7e8129214d22c3dc104
dvdcrc89@gmail.com <dvdcrc89@gmail.com>
2018-12-05 15:04:21 +0000

    Implements function for create Pilots

--- js/game/states/Game.js
+++ js/game/states/Game.js
@@ -12,10 +12,7 @@ JustRun.Game = {
         this.coins = game.add.group();
         this.players = game.add.group();
         this.shooters = new Shooters(this.game)
-        this.players.add(new Pilot(this.game, 0, game.height - 50, 'pilot', this.shooters.fireballs));
-        this.players.add(new Pilot(this.game, 0, game.height - 250, 'pilot', this.shooters.fireballs));
-        this.players.add(new Pilot(this.game, 0, game.height - 450, 'pilot', this.shooters.fireballs));
-        this.players.add(new Pilot(this.game, 0, game.height - 850, 'pilot', this.shooters.fireballs));
+        this.createPilots(4);
         this.messages = game.add.group()
         this.dead = game.add.group();
 
@@ -31,45 +28,32 @@ JustRun.Game = {
             lastKill: this.time.now,
             killNumber: 0
         };
-        this.timer2 = game.time.events.loop(1000, () => {
-            if (this.players.length < 10000) {
-                this.players.add(new Pilot(this.game, 0, (Math.random() * game.height ), 'pilot', this.shooters.fireballs))
-            }
-        }, this)
-        this.timer3 = game.time.events.loop(405, () => this.youCanShot = true, this)
-        this.timer4 = game.time.events.loop(4000, () => this.messages.children.map(t => t.destroy()), this)
+       
         this.timer6 = game.time.events.loop(10000, () => this.background.tint = Math.random() * 0xffffff, this)
 
         this.coinAudio = game.add.audio('coinAudio');
         this.ugradeAudio = game.add.audio('upgradeAudio');
         this.hitAudio = game.add.audio('hitAudio');
-        console.log(this.dog, this.player);
+        this.createPilotTimer = game.time.now;
+        this.createPilotFrequency = 2000;
     },
     update: function() {
+        if(this.createPilotTimer<game.time.now){
+            this.createPilots(1);
+            this.createPilotTimer=game.time.now+this.createPilotFrequency;
+        }
+            
         this.shooters.fireballs.children.filter((fireball) => fireball.x < 0).map((fireball) => fireball.destroy());
         game.physics.arcade.collide(this.players, this.ground);
-        game.physics.arcade.collide(this.dog, this.ground);
-
-        this.players.children.map(player => player.play());
-        this.monster.play();
-
-
-
-
-        game.physics.arcade.overlap(this.shooters.fireballs, this.players, this.die, null, this);
+        game.physics.arcade.overlap(this.shooters.fireballs, this.players, this.kill, null, this);
 
 
 
 
     },
-    die: function(fireball, player) {
+    kill: function(fireball, player) {
         if(this.time.now>this.combo.lastKill) {this.combo.lastKill= this.time.now; this.combo.killNumber= 0}
-
-        if (player.x > 50) {
-            player.animations.play("die", false);
-            player.body.velocity.x = 0;
-            player.body.velocity.y = 400;
-
+            player.die();
             this.combo.lastKill = this.time.now+1000
             this.combo.killNumber++;
             
@@ -82,17 +66,17 @@ JustRun.Game = {
 
             this.messages.removeAll();
             if (this.combo.killNumber>1)
-            this.messages.add(game.add.text(player.x, player.y, this.combo.killNumber + ' X COMBO KILL!', style));
-
-            player.rotation += 2;
-
-            player.tint = 0xF42336;
-            player.body.collideWorldBounds = false;
-            player.dead = true;
+                this.messages.add(game.add.text(player.x, player.y, this.combo.killNumber + ' X COMBO KILL!', style));
             this.players.remove(player);
             this.dead.add(player);
 
-        }
+        },
+    createPilots:function(numberOfPlayers){
+        for(let i=0;i<numberOfPlayers;i++)    
+            this.players.add(new Pilot(this.game, 0, game.height - Math.random()*850, 'pilot', this.shooters));
 
+        
     }
+
+
 };
