3326973a79a69e30cb2f654906aed3d5764e8be8
dvdcrc89@gmail.com <dvdcrc89@gmail.com>
2018-12-07 18:00:42 +0000

    Implements stage manager

--- js/game/states/Game.js
+++ js/game/states/Game.js
@@ -7,23 +7,13 @@ JustRun.Game = {
         this.gameStartAt=game.time.now;
         this.background = game.add.tileSprite(0, 0, game.width, game.height - 1, 'background');
         this.background.autoScroll(-350, 0);
-        this.ground = game.add.tileSprite(0, game.height - 1, game.width, 1, 'ground');
-        this.ground.autoScroll(-750, 0);
-        this.coins = game.add.group();
-        this.players = game.add.group();
+        this.pilots = game.add.group();
         this.shooters = new Shooters(this.game)
-        this.createPilots(10);
         this.messages = game.add.group()
         this.dead = game.add.group();
-
-        game.physics.enable([this.ground, this.players], Phaser.Physics.ARCADE)
+        this.difficulty = 0;
         game.physics.enable(this.dead, Phaser.Physics.ARCADE)
-
-        this.ground.body.collideWorldBounds = true;
-        this.ground.body.immovable = true;
-        this.ground.body.allowGravity = false;
-        this.monster = new Monster(this.game, 200, game.height, 'monster', this.players, this.shooters)
-        this.youCanShot = true;
+        this.monster = new Monster(this.game, 200, game.height, 'monster', this.pilots, this.shooters)
         this.combo = {
             lastKill: this.time.now,
             killNumber: 0
@@ -31,44 +21,29 @@ JustRun.Game = {
        
         this.timer6 = game.time.events.loop(10000, () => this.background.tint = Math.random() * 0xffffff, this)
 
-        this.coinAudio = game.add.audio('coinAudio');
         this.ugradeAudio = game.add.audio('upgradeAudio');
         this.hitAudio = game.add.audio('hitAudio');
-        this.createPilotTimer = game.time.now;
-        this.createPilotFrequency = 1000;
+      
     },
     update: function() {
-//        this.createPilotFrequency = 800 + ((this.gameStartAt - game.time.now)/30);
-//        if(this.createPilotFrequency<400) this.createPilotFrequency = 400;
-//        if(this.createPilotTimer<game.time.now && this.players.length <100){
-//            this.createPilots(1);
-//            this.createPilotTimer=game.time.now+this.createPilotFrequency;
-//        }
-        
-        
-        if(game.time.now-this.gameStartAt<20000){
-            if(this.players.length <6){
-                console.log("Step1")
-                this.createPilots(10-this.players.length,false,false);
-            }
-        } else if (game.time.now-this.gameStartAt<40000){
-            console.log("Step2")
-            if(this.players.length <4)
-               this.createPilots(6-this.players.length,true,false);
-
-        } else if (game.time.now-this.gameStartAt<90000 ){
-            console.log(this.players.length)
-                        if(this.players.length <1)
-
-               this.createPilots(4,true,true);
-        }
-        
-        
+        game.physics.arcade.collide(this.pilots, this.ground);
+        game.physics.arcade.collide(this.shooters.fireballs, this.pilots, this.kill, null, this);
+        game.physics.arcade.collide(this.shooters.bullets, this.monster, (player,bullet)=>{
+            bullet.destroy();
+            player.life--;
+        }, null, this);
+        game.physics.arcade.collide(this.pilots, this.monster,(player,pilot)=>{
+                pilot.life=1;
+                this.kill(null,pilot);
+                player.life-=5;
             
-        this.shooters.fireballs.children.filter((fireball) => fireball.x < 0).map((fireball) => fireball.destroy());
-        game.physics.arcade.collide(this.players, this.ground);
-        game.physics.arcade.overlap(this.shooters.fireballs, this.players, this.kill, null, this);
-        game.physics.arcade.overlap(this.shooters.bullets, this.monster, (player,bullet)=>bullet.kill(), null, this);
+        }, null, this);
+
+        
+        this.stageLoop();
+            
+//        this.shooters.fireballs.children.filter((fireball) => fireball.x > this.game.width).forEach((fireball) => fireball.destroy());
+   
 
 
 
@@ -76,10 +51,12 @@ JustRun.Game = {
 
     },
     kill: function(fireball, player) {
-         if(fireball!==player.hittedBy){
+         
+           
+            if(fireball && !fireball.isPerforant) {
+               fireball.kill();
+            }
             player.die();
-            player.hittedBy=fireball; 
-            console.log(player.life);
             if(player.life<1){
             if(this.time.now>this.combo.lastKill) {this.combo.lastKill= this.time.now; this.combo.killNumber= 0}
                 this.combo.lastKill = this.time.now+1000
@@ -95,15 +72,38 @@ JustRun.Game = {
                 this.messages.removeAll();
                 if (this.combo.killNumber>1)
                     this.messages.add(game.add.text(player.x-200, player.y, this.combo.killNumber + ' X HIT!', style));
-                this.players.remove(player);
+                
+                this.pilots.remove(player);
                 this.dead.add(player);
             }
-         }
         },
-    createPilots:function(numberOfPlayers,areArmed,areInteligents){
-        for(let i=0;i<numberOfPlayers;i++)    
-            this.players.add
-            (new Pilot(this.game, this.game.width, game.height/numberOfPlayers *i, 'pilot', this.shooters,this.gameStartAt,areArmed,areInteligents));
+    stageLoop: function(){
+        
+        if(game.time.now-this.gameStartAt<20000){
+            if(this.pilots.length <6+this.difficulty){
+                
+                this.createPilots(10+this.difficulty-this.pilots.length,false,false);
+            }
+        } else if (game.time.now-this.gameStartAt<40000){
+       
+            if(this.pilots.length <4+this.difficulty)
+               this.createPilots(6+this.difficulty-this.pilots.length,true,false);
+
+        } else if (game.time.now-this.gameStartAt<90000 ){
+                        if(this.pilots.length <1+this.difficulty){
+                        this.shooters.fireballs.removeAll();    
+                        this.createPilots(4+this.difficulty,true,true);
+                        }
+        } else if (game.time.now-this.gameStartAt>90000 ){
+            this.gameStartAt=game.time.now;
+            this.difficulty++;
+        }
+        
+    },
+    createPilots:function(numberOfpilots,areArmed,areInteligents){
+        for(let i=0;i<numberOfpilots;i++)    
+            this.pilots.add
+            (new Pilot(this.game, this.game.width, game.height/numberOfpilots *i, 'pilot', this.shooters,this.gameStartAt,areArmed,areInteligents));
 
         
     }
